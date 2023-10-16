56dd98e6314578e5fd5e7865c88fa056314c64ab
Ivan Sierra <sierraivan93@gmail.com>
2023-10-16 18:46:03 -0300

    Create a custom hook useWeather

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,15 @@
-import { useState } from 'react'
 import WeatherCard from "../src/components/WeatherCard.jsx"
 import Error from "../src/components/Error.jsx"
 import Footer from "../src/components/Footer.jsx"
 import Logo from './components/Logo.jsx'
-import { useCity } from "./hooks/useCity.js"
-import { fetchData } from "./services/fetchData.js"
-import { dataReorganizer } from './services/dataReorganizer.js'
+import { useWeather } from "./hooks/useWeather.js"
 
 function App() {
-  const [city, setCity] = useState('')
-  const [weather, setWeather] = useState('')
-  const [error, setError] = useState({
-    error: false,
-    message: '',
-  })
+  const { city, setCity, weather, error, handleSubmit } = useWeather()
 
-  async function onSubmit (e){
+  function onSubmit (e){
     e.preventDefault()
-    setError({
-      error: false,
-      message: '',
-    })
-    try {
-      if(!city.trim()) throw { message: 'The field city is required' }
-      const data = await fetchData(city)
-      const weatherResponse = dataReorganizer(data)
-      setWeather(weatherResponse)
-    } catch (error) {
-      setError({
-        error: true,
-        message: error.message
-      })
-    }finally{
-      setCity('')
-    }
+    handleSubmit()
   }
 
   return (
@@ -41,7 +17,6 @@ function App() {
       <h1 className='titleWeb'>Weather App</h1>
       {weather ? <WeatherCard weather={weather}/> : null}
       <div>
-
       </div>
       <div className="card">
         <form action="submit" onSubmit={onSubmit} className='form'>
--- src/hooks/useCity.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-
-export function useCity ({}) {
-    const [city, setCity] = useState('')
-    
-
-    return {city, setCity}
-}
